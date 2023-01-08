75b8c25b52e27b515b52f1a55240a6d3206c33be
kuninaa19 <minchan0527@naver.com>
2023-01-08 23:24:55 +0900

    chore(다형성처리): Shape, Figure 메서드 추가
    Figure
    - 좌표 소유여부 확인 메서드
    - 인덱스에 해당하는 좌표 가져오기 (getter)
    
    Shape
    - Figure에 추가된 메서드 오버라이딩(실질구현)
    - 생성자 추가 - 자식에게 할당되던 좌표 컬렉션 추가
    - 좌표가 추가되면서 통합 equals 메서드를 구현할 수 있게 됨

--- src/main/java/coordinate/domain/Figure.java
+++ src/main/java/coordinate/domain/Figure.java
@@ -2,6 +2,9 @@ package coordinate.domain;
 
 
 public interface Figure {
+    boolean hasPoint(int x, int y);
+
+    Point getPoint(int idx);
 
     double getArea();
 
--- src/main/java/coordinate/domain/Shape.java
+++ src/main/java/coordinate/domain/Shape.java
@@ -1,5 +1,35 @@
 package coordinate.domain;
 
-public abstract class Shape implements Figure {
+import java.util.List;
+import java.util.Objects;
 
+public abstract class Shape implements Figure {
+    private final List<Point> points;
+
+    protected Shape(List<Point> points) {
+        this.points = points;
+    }
+
+    @Override
+    public Point getPoint(int idx) {
+        return this.points.get(idx);
+    }
+
+    @Override
+    public boolean hasPoint(int x, int y) {
+        return points.stream().anyMatch(point -> point.equals(new Point(x, y)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shape shape = (Shape) o;
+        return Objects.equals(points, shape.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
 }
